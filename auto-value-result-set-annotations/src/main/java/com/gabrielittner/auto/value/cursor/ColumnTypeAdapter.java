9d3138df7110596bb19afcdd1ffad30e1bbf7ea9
themadhukiran <pmadhukiran96@gmail.com>
2016-08-08 03:28:33 +0530

    refactor module names

--- auto-value-result-set-annotations/src/main/java/com/gabrielittner/auto/value/cursor/ColumnTypeAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.gabrielittner.auto.value.cursor;
-
-
-import java.sql.ResultSet;
-
-public interface ColumnTypeAdapter<T> {
-    T fromResultSet(ResultSet resultSet, String columnName);
-}
